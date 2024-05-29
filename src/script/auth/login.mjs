95300d2b79adc32fb04e9170ba78aa52c887000f
AutopsyTurvy <102550358+AutopsyTurvy@users.noreply.github.com>
2024-05-29 19:28:01 +0200

    --Help from other students to ascertain why i get errors and they do not.

--- src/script/auth/login.mjs
+++ src/script/auth/login.mjs
@@ -4,27 +4,34 @@
 
 
 import { API_SOCIAL_URL } from "../constants.mjs";
+import * as storage from "../../storage/index.mjs";
 
 const action = "/auth/login";
 const method = "post";
 
 export async function login(profile) {
-    const loginURL = API_SOCIAL_URL + action;
-    const body = JSON.stringify(profile);
+  const loginURL = API_SOCIAL_URL + action;
+  const body = JSON.stringify(profile);
 
-    const response = await fetch(loginURL, {
-        headers: {
-            "Content-Type": "application/json"
-        },
-        method,
-        body
-    });
+  const response = await fetch(loginURL, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method,
+    body,
+  });
 
-    const result = await response.json()
-    console.log()
+  const result = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Error logging in");
+  } else {
+    storage.save("profile", result.name);
+    storage.save("token", result.accessToken);
+    console.log("token", result.accessToken);
+  }
+
+  console.log(result);
 }
-
-
-
 
 
